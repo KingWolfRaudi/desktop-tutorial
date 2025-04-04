2658a8d7a645a34b734bdecea204a437745e6b4c
Oscar Orta <99383169+KingWolfRaudi@users.noreply.github.com>
2025-04-04 03:11:18 -0400

    Agregamos Test
    Test es para los proyectos de practica y aprendizaje

---
+++ Test/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    cout<<"Hola esta es mi primera prueba"<<endl;
+    cout<<"         Hola Hola Hola"<<endl;
+    cout<<"holito de uwu sin ewe"<<endl;
+    return 0;
+}
